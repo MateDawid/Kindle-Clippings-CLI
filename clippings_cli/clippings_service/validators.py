9c6dbc98fa2c59262edaf9eb27486c1304c28722
JE01TD <dawid.mateusiak@ing.com>
2025-01-26 05:43:16 +0100

    cli-init: TestClippingServiceValidators

--- clippings_cli/clippings_service/validators.py
+++ clippings_cli/clippings_service/validators.py
@@ -2,6 +2,8 @@
 File containing data parsers for ClippingsService class.
 """
 
+MANDATORY_FIELDS = ("book", "clipping_type", "page_number", "created_at", "location", "content")
+
 
 def validate_fields(clipping: dict) -> dict:
     """
@@ -14,7 +16,7 @@ def validate_fields(clipping: dict) -> dict:
         dict: Dictionary containing errors found in Clipping dictionary.
     """
     errors = {}
-    for field in ("book", "clipping_type", "page_number", "created_at", "location", "content"):
+    for field in MANDATORY_FIELDS:
         if field not in clipping:
             errors[field] = f"Field {field} missed in Clipping."
     return errors
