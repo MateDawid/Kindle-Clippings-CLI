899e734f560aa95b3ad8bd16f335ede05f0d356b
JE01TD <dawid.mateusiak@ing.com>
2025-01-29 10:46:23 +0100

    cli-init: Test for PermissionError in Excel handlers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import shutil
+from typing import Any
 
 import pytest
 from _pytest.fixtures import SubRequest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def cleanup_temp_files(request: SubRequest):
     """
     Fixture to clean up temporary files directory created during performing tests.
@@ -18,3 +19,32 @@ def cleanup_temp_files(request: SubRequest):
         shutil.rmtree(temp_dir, ignore_errors=True)
 
     request.addfinalizer(remove_temp_files)
+
+
+@pytest.fixture
+def clippings() -> list[dict[str, Any]]:
+    """
+
+    #TODO
+
+    """
+    return [
+        {
+            "book": {"title": "Book 1", "author": "Author 1"},
+            "content": "Content 1",
+            "page_number": "1",
+            "location": "123",
+            "created_at": "2025-01-01",
+            "clipping_type": "Highlight",
+            "errors": {},
+        },
+        {
+            "book": {"title": "Book 2", "author": "Author 2"},
+            "content": "Content 2",
+            "page_number": "2",
+            "location": "456",
+            "created_at": "2025-01-02",
+            "clipping_type": "Note",
+            "errors": {},
+        },
+    ]
